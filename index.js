3d9343ff94d8b01f7d1bd1c63799cd8c865c2a79
Sergey87 <serge.techno@gmail.com>
2022-11-05 19:55:47 +0300

    1 task

---
+++ index.js
@@ -0,0 +1,31 @@
+// 1 - Задача. Условие: Найти самый маленький элемент - число в массиве
+
+findSmallestEl = (arg) => {
+  /*  
+
+  1 - способ: Math.min() - не принимает массив => ...spread (раздел массив на эл)
+    return Math.min(...arg);
+
+  */
+
+  // 2 - способ: лучше + for
+
+  let currentMin = arg[0];
+
+  // i = 1 !!!
+  for (let i = 1; i < arg.length; i++) {
+    if (arg[i] < currentMin) {
+      currentMin = arg[i];
+    }
+  }
+
+  return currentMin;
+};
+
+/* --- test
+console.log(findSmallestEl([55, 77, 2, 555, 1, 9])); // 1
+console.log(findSmallestEl([55, 77, 222, 555, 4, 9])); // 4
+console.log(findSmallestEl([55, 77, 10, 555, 12, 19])); // 10
+*/
+
+// 2 - Задача. Условие:
