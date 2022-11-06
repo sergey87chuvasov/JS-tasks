f0dff3670eeaea1491fe67892bf02f02bbcd3b2e
Sergey87 <serge.techno@gmail.com>
2022-11-06 18:45:34 +0300

    added 2 task

--- index.js
+++ index.js
@@ -28,4 +28,27 @@ console.log(findSmallestEl([55, 77, 222, 555, 4, 9])); // 4
 console.log(findSmallestEl([55, 77, 10, 555, 12, 19])); // 10
 */
 
-// 2 - Задача. Условие:
+// 2 - Задача. Условие: Найти среднее арифметическое в массиве - округленное вниз до ближайшего целого числа - Math.floor()
+
+function getAverage(arr) {
+  /* 
+
+  // 1 - способ - reduce
+  return Math.floor(arr.reduce((acc, curElem) => acc + curElem) / arr.length);
+
+  */
+
+  // 2 - способ:  forEach или for
+
+  let acc = 0;
+  arr.forEach((el) => {
+    acc += el;
+  });
+  return Math.floor(acc / arr.length);
+}
+
+/* --- test
+console.log(getAverage([51, 79, 2, 595, 1, 9])); // 122
+console.log(getAverage([5, 77, 22, 55, 4, 9])); // 28
+console.log(getAverage([55, 7, 10, 5, 1, 1])); // 13
+*/
