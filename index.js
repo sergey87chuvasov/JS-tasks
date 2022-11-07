9ebf126e1002774e3beb3cc614b853ec701083d1
Sergey87 <serge.techno@gmail.com>
2022-11-07 16:18:51 +0300

    added 2 tasks

--- index.js
+++ index.js
@@ -52,3 +52,42 @@ console.log(getAverage([51, 79, 2, 595, 1, 9])); // 122
 console.log(getAverage([5, 77, 22, 55, 4, 9])); // 28
 console.log(getAverage([55, 7, 10, 5, 1, 1])); // 13
 */
+
+// 3 - Задача. Условие: Развернуть последовательность и вернуть массив чисел
+
+// 1 - способ - for + push()
+const reverseSeq = (n) => {
+  const arr = [];
+
+  for (let i = n; i > 0; i--) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+/*  --- test
+console.log(reverseSeq(5)); // [ 5, 4, 3, 2, 1 ]
+console.log(reverseSeq(3)); // [ 3, 2, 1 ]
+console.log(reverseSeq(9)); // [ 9, 8, 7, 6, 5, 4, 3, 2, 1]
+*/
+
+// 4 - Задача. Условие: вывести определнные числа исходя из условия - if, else if
+
+function finalGrade(e, p) {
+  if (e > 90 || p > 10) {
+    return 100;
+  } else if (e > 75 && p >= 5) {
+    return 90;
+  } else if (e > 50 && p >= 2) {
+    return 75;
+  } else {
+    return 0;
+  }
+}
+
+/*  --- test
+console.log(finalGrade(100, 12)); // 100
+console.log(finalGrade(85, 5)); // 90
+console.log(finalGrade(60, 3)); // 75
+console.log(finalGrade(30, 1)); // 0
+*/
