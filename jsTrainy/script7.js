f425ec15696a5a4d938b758663efc602ea9d9efc
Sergey87 <serge.techno@gmail.com>
2023-04-04 23:11:57 +0300

    added js trainy - opp

--- jsTrainy/script7.js
+++ jsTrainy/script7.js
@@ -112,3 +112,174 @@ cart.decreaseAmount(1);
 console.log(cart); // Cart { products: [] }
 
 // ЦЕПОЧКИ PROTOTYPE
+
+// КЛАССЫ - лучше использовать - синт сахар
+// у классов хостинга нет, тело класса всегда в use strict даже если он не объявлен
+
+class BookClass {
+  isRead = false;
+  constructor(title, author) {
+    this.author = author;
+    this.title = title;
+  }
+
+  read() {
+    this.isRead = true;
+  }
+}
+
+const lordOfTheRing2 = new BookClass('lost', 'Tolkien');
+console.log(lordOfTheRing2); // BookClass { isRead: false, author: 'Tolkien', title: 'lost' }
+// !!!
+console.log(lordOfTheRing2.__proto__); // {constructor: ƒ, read: ƒ}
+console.log(lordOfTheRing2 instanceof BookClass); // true
+lordOfTheRing2.read();
+console.log(lordOfTheRing2); // BookClass { isRead: true, author: 'Tolkien', title: 'lost' }
+
+// getters and setters - возможность использовать свойства как методы - но называя как свойство - изменение внутренней логики присвоения и получения что бы что-то поменять
+// РАБОТА В ОБЪЕКТАХ
+const task = {
+  title: 'Task1',
+  dueTo: new Date('2023/01/01'),
+
+  // метод - проверки просрочки
+  isOverdue() {
+    return this.dueTo < new Date();
+  },
+
+  // геттер - получить значение - обращение как к свойству а не как к функции - по сути это некоторое скрытие за собой реализации получения некторых данных
+  // сеттер установить значение - сеттер всегда принимает одно значение - то чему оно должго быть присовено
+
+  get isOverdue2() {
+    return this.dueTo < new Date();
+  },
+
+  set isOverdue3(isOverdueTask) {
+    if (!isOverdueTask) {
+      this.dueTo = new Date();
+    }
+  },
+};
+
+console.log(task.isOverdue()); // true
+console.log(task.isOverdue2); // true - геттер
+task.isOverdue3 = false;
+console.log(task); // {title: 'Task1', dueTo: Tue Apr 04 2023 21:57:31 GMT+0300 (Москва, стандартное время), isOverdue: ƒ}
+
+// геттер и сеттер так же работают и в классах
+// РАБОТА В КЛАССАХ (запятых нет)
+
+class Task {
+  constructor(title, dueDate) {
+    this.title = title;
+    this.dueDate = dueDate;
+  }
+
+  get isOverdue() {
+    return this.dueTo < new Date();
+  }
+
+  set dueDate(date) {
+    if (date < new Date()) {
+      return;
+    }
+    this._dueDate = date;
+  }
+}
+
+const newTask = new Task('Task2', new Date());
+console.log(newTask.isOverdue); // false
+console.log((newTask.dueDate = new Date('2024/1/1')));
+console.log(newTask);
+
+// Static
+
+// статичное св-во числа
+Number.MAX_SAFE_INTEGER; // статичн метод
+new Number();
+
+class Test {
+  static a = 1; // статическое св-во
+
+  static hello() {
+    console.log('Hello'); // статический метод
+  }
+
+  // статичный блок
+  static {
+    let b = 5;
+    this.a = 5;
+  }
+}
+
+Test.hello(); // Hello
+console.log(Test.a); // 5 (без блока a = 1)
+
+// Приватные методы и св-ва  # - доступные только внутри
+
+class Car {
+  #vin = 6;
+
+  #changeVin() {
+    console.log('changed');
+  }
+
+  test() {
+    this.#changeVin();
+  }
+
+  static #field = 3;
+}
+
+const car = new Car();
+// car.   - нет св-в
+car.test(); // changed
+// car. field - нет такого метода
+
+// УПРАЖНЕНИЕ - КЛАСС ПОЛЬЗОВАТЕЛЯ реализ (логин и пароль)
+
+class UserX {
+  // созд приватные поля
+  #login;
+  #_password;
+
+  constructor(login, password) {
+    this.#login = login;
+    this.#password = password;
+  }
+
+  set #password(pass) {
+    this.#_password = pass.split('').reverse().join('');
+  }
+
+  get #password() {
+    return this.#_password.split('').reverse().join('');
+  }
+
+  get login() {
+    return this.#login;
+  }
+
+  // методы
+  checkPassword(pass) {
+    return this.#password === pass;
+  }
+
+  changePassword(oldPass, newPass) {
+    if (!this.checkPassword(oldPass)) {
+      return false;
+    }
+    this.#password = newPass;
+    return true;
+  }
+}
+
+const userX = new UserX('aa@aa.ru', '12345');
+// console.log(userX); // UserX {#login: 'aa@aa.ru', #_password: '5,4,3,2,1'}
+// console.log(userX.login);
+// userX.login = '111@tut.by';
+// console.log(userX); // не можем поменять логин
+console.log(userX.checkPassword('12345')); // true
+console.log(userX.checkPassword('7777')); // false
+console.log(userX.changePassword('12345', '99999')); // true
+console.log(userX); // UserX {#login: 'aa@aa.ru', #_password: '99999'}
